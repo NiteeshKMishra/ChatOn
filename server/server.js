43d78d4f4398384cbb20ca12cca925fa8f598a3c
iamniteeshmishra@gmail.com <nitish219@gmail.com>
2019-02-19 10:44:10 +0530

    socketio added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
 
 const clientPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
@@ -8,7 +10,17 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(clientPath));
 
+var server = http.createServer(app);
 
-app.listen(port, () => {
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log("New Client Connected")
+
+  socket.on('disconnect', () => {
+    console.log("client is disconnected");
+  });
+});
+server.listen(port, () => {
   console.log(`Server is up at ${port}`);
 })
